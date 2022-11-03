8127937796df3a2e3da74eee23b26487725fdfda
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-11-03 18:01:47 +0530

    tooltips!!

--- src/main/java/com/beastnighttv/jak/block/custom/JumpyBlock.java
+++ src/main/java/com/beastnighttv/jak/block/custom/JumpyBlock.java
@@ -1,10 +1,20 @@
 package com.beastnighttv.jak.block.custom;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -12,6 +22,17 @@ import net.minecraft.world.level.block.state.BlockState;
 public class JumpyBlock extends Block {
     public JumpyBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable BlockGetter blockGetter, List<Component> components, TooltipFlag flag) {
+        if (Screen.hasShiftDown()) {
+            components.add(Component.literal("Stand on it to get Jump Boost!!").withStyle(ChatFormatting.AQUA));
+        } else {
+            components.add(Component.literal("Press SHIFT for more info").withStyle(ChatFormatting.YELLOW));
+        }
+
+        super.appendHoverText(itemStack, blockGetter, components, flag);
     }
 
     @Override
--- src/main/java/com/beastnighttv/jak/item/custom/EightBallItem.java
+++ src/main/java/com/beastnighttv/jak/item/custom/EightBallItem.java
@@ -1,5 +1,11 @@
 package com.beastnighttv.jak.item.custom;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
@@ -7,6 +13,7 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 
 public class EightBallItem extends Item {
@@ -36,6 +43,17 @@ public class EightBallItem extends Item {
 
     public EightBallItem(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+        if (Screen.hasShiftDown()) {
+            components.add(Component.literal("Right click to get random response!!").withStyle(ChatFormatting.AQUA));
+        } else {
+            components.add(Component.literal("Press SHIFT for more info").withStyle(ChatFormatting.YELLOW));
+        }
+
+        super.appendHoverText(itemStack, level, components, flag);
     }
 
     @Override
